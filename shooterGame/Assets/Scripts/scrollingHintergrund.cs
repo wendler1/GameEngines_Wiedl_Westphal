458f171c374cb0857a960e029dd9d1ca694b0fc4
wendler1 <andreas.wiedl90@gmail.com>
2018-11-21 14:38:41 +0100

    backgrounds

---
+++ shooterGame/Assets/Scripts/scrollingHintergrund.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingHintergrund : MonoBehaviour {
+
+	float scrollSpeed = -5f;
+	Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPos = Mathf.Repeat (Time.time * scrollSpeed, 17.64f);
+		transform.position = startPos + Vector2.right * newPos;
+	}
+}
