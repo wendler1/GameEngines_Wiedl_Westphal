8f8cc730a3d11b093d7e3b18708e1215a8c68816
wendler1 <andreas.wiedl90@gmail.com>
2018-11-24 12:49:59 +0100

    MainMenu build

---
+++ shooterGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public string starteScene0;
+
+	public void playGame () {
+		Application.LoadLevel(starteScene0);
+	}
+
+	public void endGame () {
+		Application.Quit();
+	}
+}
