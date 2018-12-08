f3d6f6e2fb55844ee40171ea53468f4aa0353b20
wendler1 <andreas.wiedl90@gmail.com>
2018-12-08 14:06:49 +0100

    mo money mo problems

---
+++ shooterGame/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MoneyManager : MonoBehaviour {
+
+	public Text MoneyAnzeige;
+	public static int coinAmount;
+
+	void Start ()
+	{
+		MoneyAnzeige = GetComponent<Text>();
+	}
+
+	void Update () 
+	{
+		MoneyAnzeige.text = "$: " + coinAmount.ToString();
+	}
+
+	
+}
